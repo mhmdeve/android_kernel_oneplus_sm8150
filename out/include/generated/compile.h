68c4477e4752814be77abd7998b8cee3a7a607dc
Master <mhmdeveloperofficial@gmail.com>
2023-08-27 17:27:39 +0500

    tmp

--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 2 */
+/* This file is auto generated, version 3 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "aarch64"
-#define UTS_VERSION "#2 SMP PREEMPT Sun Aug 27 15:45:32 PKT 2023"
+#define UTS_VERSION "#3 SMP PREEMPT Sun Aug 27 17:11:07 PKT 2023"
 #define LINUX_COMPILE_BY "Master"
 #define LINUX_COMPILE_HOST "PAK"
 #define LINUX_COMPILER "Android (9597734, based on r487747) clang version 17.0.0 (https://android.googlesource.com/toolchain/llvm-project d9f89f4d16663d5012e5c09495f3b30ece3d2362), GNU ld (binutils-2.27-bd24d23f) 2.27.0.20170315"
