0cf30a0c00280e79eba0832d9626112ef5b42d0a
Master <mhmdeveloperofficial@gmail.com>
2023-08-28 12:00:43 +0500

    tmp

--- drivers/media/platform/msm/camera/cam_core/cam_context.h
+++ drivers/media/platform/msm/camera/cam_core/cam_context.h
@@ -16,8 +16,9 @@
 #include <linux/mutex.h>
 #include <linux/spinlock.h>
 #include <linux/kref.h>
-#include "cam_req_mgr_interface.h"
+#include "../cam_req_mgr/cam_req_mgr_interface.h"
 #include "cam_hw_mgr_intf.h"
+#include "cam_context.h"
 
 /* Forward declarations */
 struct cam_context;
